75aaf40ff978f2d5d4db6edfd1c142d355cd577d
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-06-25 18:13:20 +0530

    1st commit

---
+++ GitJenkinsTest/src/test/java/GitJenkinsTest/GitJenkinsTest/Test.java
@@ -0,0 +1,14 @@
+package GitJenkinsTest.GitJenkinsTest;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to git and jenkins");
+	}
+
+}
